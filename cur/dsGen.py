712a0a18f29afe6ed533fc9ac6fce1ba0fce2af6
Von <haodong.lu.me@gmail.com>
2025-02-27 19:13:36 +0800

    data compression

--- cur/dsGen.py
+++ cur/dsGen.py
@@ -3,8 +3,10 @@ import torch
 import re
 import os
 import math
+import random
 from tqdm import tqdm
 import numpy as np
+from scipy import sparse
 import matplotlib.pyplot as plt
 from torch.nn import functional as F
 from transformers import AutoTokenizer, AutoModelForCausalLM, utils
@@ -15,7 +17,7 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True,garbage_collec
 utils.logging.set_verbosity_error()  # Suppress standard warnings
 
 models = [
-        # 'meta-llama/Llama-3.2-1B', # Llama-3.2-1B (16 layers 32 heads)
+        'meta-llama/Llama-3.2-1B', # Llama-3.2-1B (16 layers 32 heads)
         'meta-llama/Meta-Llama-3-8B', # Meta-Llama-3-8B
         'meta-llama/Meta-Llama-3-70B', # Meta-Llama-3-70B
         'meta-llama/Meta-Llama-3-8B-Instruct', # Meta-Llama-3-8B-Instruct (the only model TRAIL tests with)
@@ -41,13 +43,13 @@ datasets = {
 
 SPECIAL_LABEL = -1
 CACHE_DIR = "./.cache/huggingface/datasets"
-FEATURE_DIR = "./training_data/features/llma3_8b_instruct"
-METADATA_DIR = "./training_data/metadata/llma3_8b_instruct"
+FEATURE_DIR = "./training_data/features/llma3_1b"
+METADATA_DIR = "./training_data/metadata/llma3_1b"
 DS_NAME = datasets[4]
 
 temperatures = [0.1, 0.3, 0.5, 0.9] # low, mid, high creativity
 top_k_values = [1, 5, 10] # low, mid, high diversity
-repetition_penalties = [1.0, 1.3, 1.6] # low, mid, high coherence
+repetition_penalties = [1.3, 1.6] # low, mid, high coherence
 max_new_tokens_values = [300, 500] # low, mid, high length
 # system_parameters = []
 # for temp in temperatures:
@@ -101,7 +103,7 @@ data = {
 # print("Choose the model to test:")
 # for i, model in enumerate(models, 1):
 #     print(f" {i}. {model.split('/')[-1]}")
-model_choice = 3
+model_choice = 1
 # if model_choice < 1 or model_choice > len(models):
 #     raise ValueError(f"Invalid model choice. Please enter a number between 1 and {len(models)}")
 model_name = models[model_choice - 1]
@@ -127,6 +129,87 @@ def sample_top_k(logits, k, temperature):
     probs = F.softmax(top_k_logits, dim=-1)
     return top_k_indices[0, torch.multinomial(probs, 1).item()]
 
+def commpress_attention_matrix(attention_matrix):
+    """
+    half precision
+    sparse matrix
+    Not yet: Compress the attention matrix by removing the diagonal and upper triangular part
+    Not yet: SVG compression
+    """
+    seq_len = attention_matrix.shape[0]
+    mask = np.tri(seq_len, dtype=bool)
+    attention_matrix = attention_matrix * mask
+    
+    attention_matrix = attention_matrix.astype(np.float16)
+    sparse_matrix = sparse.coo_matrix(attention_matrix)
+    return sparse_matrix
+    
+    # seq_len = attention_matrix.shape[0]
+    # mask = np.triu(np.ones((seq_len, seq_len)), k=0)
+    # attention_matrix = attention_matrix * (1 - mask)
+    
+    # return {
+    #     'data': sparse_matrix.data,
+    #     'row': sparse_matrix.row,
+    #     'col': sparse_matrix.col,
+    #     'shape': sparse_matrix.shape
+    # }
+
+def encode_params(params):
+    """
+    Encode system parameters into a numpy array / 32-bit integer
+    """
+    # return np.array([
+    #     np.interp(params['temperature'], [0.1, 0.9], [0, 255]),
+    #     params['top_k'],
+    #     np.interp(params['repetition_penalty'], [1.0, 1.6], [0, 255]),
+    #     params['max_seq_len'] // 100
+    # ], dtype=np.uint8)
+    assert 0.1 <= params['temperature'] <= 0.9
+    assert 1 <= params['top_k'] <= 255
+    assert 1.0 <= params['repetition_penalty'] <= 1.6
+    assert 100 <= params['max_seq_len'] <= 25500
+    
+    # encode
+    temp_enc = int(np.interp(params['temperature'], [0.1, 0.9], [0, 255]))
+    topk_enc = params['top_k']
+    rep_enc = int(np.interp(params['repetition_penalty'], [1.0, 1.6], [0, 255]))
+    maxlen_enc = params['max_seq_len'] // 100
+    
+    return np.uint32(
+        (temp_enc << 24) | 
+        (topk_enc << 16) | 
+        (rep_enc << 8) | 
+        maxlen_enc
+    )
+
+def decode_params(encoded):
+    """
+    Decode system parameters from a numpy array / 32-bit integer
+    """
+    return {
+        'temperature': ((encoded >> 24) & 0xFF) / 255 * 0.8 + 0.1,
+        'top_k': (encoded >> 16) & 0xFF,
+        'repetition_penalty': ((encoded >> 8) & 0xFF) / 255 * 0.6 + 1.0,
+        'max_seq_len': (encoded & 0xFF) * 100
+    }
+
+def head_selection():
+    """
+    Select a head from the model, randomly chose about 25% of the heads
+    """
+    return random.random() < 0.25
+
+def layer_selection(layer_idx):
+    """s
+    for deeper layers, have a higher chance to be selected
+    """
+    total_layers = model.config.num_hidden_layers
+    if layer_idx >= total_layers * 0.5:
+        return True
+    return random.random() < 0.5
+    
+    
 # ======================
 # Generate training data
 # ======================
@@ -225,10 +308,14 @@ def ds_generator(model, tokenizer, data, device):
                     
                     # attention matrix extraction
                     for layer_idx in range(num_layers):
+                        if not layer_selection(layer_idx):
+                            continue
                         # shape: (batch_size=1, num_heads, seq_len, seq_len)
                         layer_attn = outputs.attentions[layer_idx]
                         
                         for head_idx in range(num_heads):
+                            if not head_selection():
+                                continue
                             # shape: (seq_len, seq_len)
                             attn_matrix = layer_attn[0, head_idx].cpu().numpy()
                             
@@ -311,20 +398,48 @@ def ds_generator(model, tokenizer, data, device):
                     model_name = model.config.name_or_path.replace('/', '_')
                     filename = f"{model_name}_temp{param['temperature']}_topk{param['top_k']}_rep{param['repetition_penalty']}_seq{qa_idx}.npz"
                     
-                    # feature data saving
+                    # # feature data saving
+                    # # np.savez_compressed(
+                    # #     os.path.join(FEATURE_DIR, filename),
+                    # #     data=feature_data
+                    # # )
                     # np.savez_compressed(
                     #     os.path.join(FEATURE_DIR, filename),
-                    #     data=feature_data
+                    #     system_params=param,
+                    #     model_arch={
+                    #         "num_layers": num_layers,
+                    #         "num_heads": num_heads
+                    #     },
+                    #     features=features,
+                    #     labels=labels
                     # )
+                    compressed_features = []
+                    for feature in features:
+                        compressed_attn = commpress_attention_matrix(feature["attention_matrix"])
+                        encoded_params = encode_params(feature["system_params"])
+                        
+                        compressed_feature = {
+                            "system_params": encoded_params,
+                            "model_arch": feature["model_arch"],
+                            "layer": np.uint8(feature["layer"]), # 0-15
+                            "head": np.uint8(feature["head"]), # 0-31
+                            # 'attn_data': compressed_attn['data'],
+                            # 'attn_row': compressed_attn['row'].astype(np.uint16),
+                            # 'attn_col': compressed_attn['col'].astype(np.uint16),
+                            # 'attn_shape': (np.uint16(compressed_attn['seq_len']),) * 2,
+                            "attn_data": compressed_attn.data,
+                            "seq_pos": np.uint16(feature["seq_pos"]) # 0-25500
+                        }
+                        compressed_features.append(compressed_feature)
+                    
                     np.savez_compressed(
                         os.path.join(FEATURE_DIR, filename),
-                        system_params=param,
-                        model_arch={
-                            "num_layers": num_layers,
-                            "num_heads": num_heads
-                        },
-                        features=features,
-                        labels=labels
+                        features=np.array(compressed_features, dtype=object),
+                        labels=labels,
+                        param_interp_ranges={
+                            'temperature': [0.1, 0.9],
+                            'rep_penalty': [1.0, 1.6]
+                        }
                     )
 
                     # metadata saving
